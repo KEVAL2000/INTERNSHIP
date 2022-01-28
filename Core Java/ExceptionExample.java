ed91aaadd5666c0808b3932d60a1842b9bbf3665
KEVAL2000 <kevalparmar3233@gmail.com>
2022-01-28 19:22:51 +0530

    Put space after comma in Array

--- Core Java/ExceptionExample.java
+++ Core Java/ExceptionExample.java
@@ -9,7 +9,7 @@ public class ExceptionExample {
 
 	public static void main (String[] args) {
 		try { // Test block of code
-			int numbers[] = {1,2,3};
+			int numbers[] = {1, 2, 3};
 			System.out.println(numbers[10]);
 		} catch (Exception e) { // Catch exception
 			System.out.println(e);

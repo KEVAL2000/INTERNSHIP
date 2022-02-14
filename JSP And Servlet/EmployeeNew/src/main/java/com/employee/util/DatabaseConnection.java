23b2f650e7dcd905922551db1cfe5e2d0ae7a825
KEVAL2000 <kevalparmar3233@gmail.com>
2022-02-14 16:31:22 +0530

    Today changes are committed

--- JSP And Servlet/EmployeeNew/src/main/java/com/employee/util/DatabaseConnection.java
+++ JSP And Servlet/EmployeeNew/src/main/java/com/employee/util/DatabaseConnection.java
@@ -6,16 +6,12 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-	public static Connection createConnection() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+	public static Connection createConnection() throws SQLException {
 		Connection connection = null;
 		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedb", "root", "root");
-		} catch (SQLException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		return connection;
--- JSP And Servlet/EmployeeNew/src/main/webapp/registrationform.jsp
+++ JSP And Servlet/EmployeeNew/src/main/webapp/registrationform.jsp
@@ -4,11 +4,11 @@
 <html>
 
 <head>
-<meta charset="ISO-8859-1">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Employee Registration Form</title>
-<link rel="stylesheet" href="registrationform.css" />
+	<meta charset="ISO-8859-1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Employee Registration Form</title>
+	<link rel="stylesheet" href="registrationform.css" />
 </head>
 
 <body>
